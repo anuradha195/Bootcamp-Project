3bf3db52c70858ba195c47241d5d6d7e310180f9
anuradha <ganuradha1991@gmail.com>
2019-07-19 20:15:47 +0300

    Final Project submission

--- src/main/java/com/BootcampProject/BootcampProjectApplication.java
+++ src/main/java/com/BootcampProject/BootcampProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.BootcampProject;
+package com.BootcampProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootcampProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootcampProjectApplication.class, args);
-		//https://BootcampProject.com
-	} 
 
-}
+		SpringApplication.run(BootcampProjectApplication.class, args);
+
+	}//end main
+}//end class
--- src/test/java/com/BootcampProject/BootcampProjectApplicationTests.java
+++ src/test/java/com/BootcampProject/BootcampProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.BootcampProject;
+package com.BootcampProject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
